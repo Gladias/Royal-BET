2b6acb2d7fd8ab7e03d6af000c0db83c02a98a7f
Gladias <alek.drozdz@gmail.com>
2021-03-10 19:15:40 +0100

    Initial commit with project structure for client and server

---
+++ server/src/main/java/com/gladias/royalbet/RoyalBetApplication.java
@@ -0,0 +1,13 @@
+package com.gladias.royalbet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyalBetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoyalBetApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/gladias/royalbet/RoyalBetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gladias.royalbet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoyalBetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
