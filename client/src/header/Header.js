a3ee087b990c7bcb3aaa144be03c21da1cc07cb4
Gladias <alek.drozdz@gmail.com>
2021-03-15 17:10:33 +0100

    Delete initial react starting page and add simple views header

---
+++ client/src/header/Header.js
@@ -0,0 +1,15 @@
+import './Header.css';
+import logo from './logo.png';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Header() {
+    return (
+        <Navbar expand="md">
+            <Navbar.Brand href="#">
+                <img src={logo} className="logo" alt="logo" />
+            </Navbar.Brand> 
+        </Navbar>
+    );
+}
+
+export default Header;
